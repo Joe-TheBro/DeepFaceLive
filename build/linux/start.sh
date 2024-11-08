60c6ea191c5d3754d0e6a824382bf78002fb72dd
Joe-TheBro <hammj00@gmail.com>
2024-11-08 13:11:23 -0600

    share port

--- build/linux/start.sh
+++ build/linux/start.sh
@@ -4,4 +4,4 @@ NV_LIB=$(locate nvidia.ko |grep $(uname -r) |grep dkms | head -1)
 NV_VER=$(modinfo $NV_LIB | grep ^version |awk '{print $2}'|awk -F '.' '{print $1}')
 
 docker build . -t deepfacelive --build-arg NV_VER=$NV_VER
-docker run -d --ipc host --gpus all  -v ../../data/ --device=/dev/video0:/dev/video0 --rm deepfacelive
+docker run -d --ipc host --gpus all  -v ../../data/ -p 1234:1234 --device=/dev/video0:/dev/video0 --rm deepfacelive
